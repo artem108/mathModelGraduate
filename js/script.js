a826d19c2d4b6ca537c0cb17e9957171dfcbd6a2
artem108 <artem.arshaniy108@gmail.com>
2018-05-30 22:36:07 +0300

    Get ok K

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
+
 const arrPrevL = [2]
 let count = 0
-let arrF1 = [2]
-
+let arrF1 = [1]
+const Fout = 2
 // обьем смесителя метров
 const S = 2
-
 // мин и макс уровень в смешивателе литров
-const Lmin = 2
-const Lmax = 8
-
+const Lmin = 54
+const Lmax = 112
 //оптимальный уровень
 const Lopt = (Lmin + Lmax) / 2
 
@@ -17,36 +16,41 @@ const Lopt = (Lmin + Lmax) / 2
     let f1 = arrF1[count]//будет меняться в зависимости от уровня
     const f2 = f1 * 2
     const f3 = f1 * 4
+    let Fall = f1 * 7
+
 
     // предыдущий уровень
-    let Lprev = arrPrevL[count] // надо будет менять
-
+    let Lprev = arrPrevL[count]
     // текущий уровень
-    let Lcurr = Lprev + (f1 + f2 + f3 - Lopt) * S
+    let Lcurr = Lprev + (Fall - Fout) * S
 
 
-    f1 < 0 ? f1 = f1 = f1 * (-1) : f1 = f1
+    let k
+    k = Fall / (Lopt - Lprev)
 
-    if (Lcurr === Lopt || Lcurr > Lopt - 1 && Lcurr < Lopt + 1) {
-        setValue(f1, Lprev)
-    }  else {
-        if (Lcurr > Lopt) {
-          setValue(f1 -= 1, Lcurr)
-        } else {
-          setValue(f1 += 1, Lcurr)
-        }
-      }
+    Fall = k * (Lopt - Lprev)
 
-    console.log('Lopt: ', Lopt);
-    console.log('Lcurr: ', Lcurr);
-    console.log('f1: ', f1);
-    console.log('_______________');
+    console.log(Fall == k * (Lopt - Lprev)); //!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    if (Fall == k * (Lopt - Lprev)) setValue(Fall / 7, Lcurr)
+
+
+
+
+    // console.log(k * (Lopt - Lprev));
+    // console.log(Fall);
+
+    console.log('F!', f1);
+    // console.log('Lopt', Lopt);
+    // console.log('Lcurr', Lcurr);
+    // console.log('____________');
 
   }, 1000)
 
 const setValue = (currF1, L) => {
     arrF1.push(currF1)
     arrPrevL.push(L)
+    count ++
     createChart()
 }
 
@@ -81,5 +85,4 @@ let myChart = new Chart(ctx, {
         }]
     },
 });
-  count ++
 }
